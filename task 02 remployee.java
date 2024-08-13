800b3d6af5c38138e7411676186968753a22a0a1
mahat07 <140910546+mahat07@users.noreply.github.com>
2024-08-13 13:43:41 +0530

    Create task 02 remployee.java

---
+++ task 02 remployee.java
@@ -0,0 +1,21 @@
+package com.example.employeemanagementsystem.model;
+
+import lombok.Data;
+import javax.persistence.*;
+
+@Entity
+@Table(name = "employees")
+@Data
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+
+    @ManyToOne
+    @JoinColumn(name = "department_id")
+    private Department department;
+}
