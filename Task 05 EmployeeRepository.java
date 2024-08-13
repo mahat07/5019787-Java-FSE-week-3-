e2826a3e9f7548b5c91cbb1cae915f3b4b9392f6
mahat07 <140910546+mahat07@users.noreply.github.com>
2024-08-13 13:47:23 +0530

    Create Task 05 EmployeeRepository.java

---
+++ Task 05 EmployeeRepository.java
@@ -0,0 +1,15 @@
+package com.example.employeemanagementsystem.repository;
+
+import com.example.employeemanagementsystem.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+    List<Employee> findByName(String name);
+
+    List<Employee> findByEmail(String email);
+}
