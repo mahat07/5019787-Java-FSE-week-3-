81d8780e4a25f5b152eeb5183656606b0206a494
mahat07 <140910546+mahat07@users.noreply.github.com>
2024-08-13 13:49:09 +0530

    Create task 05 EmployeeRepository.java

---
+++ task 05 EmployeeRepository.java
@@ -0,0 +1,17 @@
+package com.example.employeemanagementsystem.repository;
+
+import com.example.employeemanagementsystem.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.NamedQuery;
+import java.util.List;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+    @NamedQuery(name = "Employee.findByName", query = "SELECT e FROM Employee e WHERE e.name = :name")
+    List<Employee> findByName(String name);
+
+    List<Employee> findByEmail(String email);
+}
